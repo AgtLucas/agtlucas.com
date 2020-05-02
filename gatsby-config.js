a76cd879012f0b7aefbe6821ab63c2508a1e2df8
Lucas <agtlucas@me.com>
2020-05-02 14:38:08 -0300

    🖼

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lucas da Silva`,
+    description: `Art portfolio.`,
+    author: `@_agtlucas`,
+    social: [
+      {
+        name: "ArtStation",
+        url: "https://www.artstation.com/agtlucas",
+      },
+      {
+        name: "Instagram",
+        url: "https://www.instagram.com/_agtlucas/",
+      },
+      {
+        name: "Twitter",
+        url: "https://twitter.com/_agtlucas",
+      },
+    ],
   },
   plugins: [
+    `gatsby-theme-gallery`,
+    `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -15,18 +31,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -29,16 +29,12 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: "90vw",
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <footer>© {new Date().getFullYear()}</footer>
       </div>
     </>
   )
